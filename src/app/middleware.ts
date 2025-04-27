aa6017b39fc7801555cc33495c2f688bd247ff67
znltto <arthur.fernandes1@uscsonline.com.br>
2025-04-27 11:59:15 -0300

    att

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -1,10 +1,13 @@
+// middleware.ts
 import { NextRequest, NextResponse } from 'next/server';
 import { verifyToken } from '../../lib/auth';
 
 export function middleware(req: NextRequest) {
   const token = req.cookies.get('token')?.value;
+  console.log('Middleware - Token:', token);
 
   if (!token || !verifyToken(token)) {
+    console.log('Middleware - Redirecionando para /');
     return NextResponse.redirect(new URL('/', req.url));
   }
 
